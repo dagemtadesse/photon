6832193ca8cbdfb4f1fc5e0aba7a7da6b8842bea
dagemtadesse <dagem@gmail.com>
2021-12-31 16:54:16 +0300

    implemented logout

--- controller/auth/logout.go
+++ controller/auth/logout.go
@@ -1,7 +1,15 @@
 package auth
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
 
 func LogoutHandler(ctx *gin.Context) {
+	// clear the cookie
+	sessions.Default(ctx).Clear()
 
+	ctx.Status(http.StatusOK)
 }
